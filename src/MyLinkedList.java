5bbf615af9b816ee0a31ae59b10cf1f98ed8ff30
Faye-yufan <yufanfei@usc.edu>
2022-12-29 20:04:08 -0800

    Update MyLinkedList class

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 public class MyLinkedList<E> {
 
     private static class Node<E> {
@@ -7,12 +9,13 @@ public class MyLinkedList<E> {
         Node(E val) { this.val = val; }
     }
 
-    private final Node<E> head;
+    private Node<E> head, tail;
     private int size;
 
     // initialize head node
     public MyLinkedList() {
         this.head = new Node<>(null);
+        this.tail = new Node<>(null);
         head.next = null;
 
         this.size = 0;
@@ -22,34 +25,47 @@ public class MyLinkedList<E> {
 
     public boolean empty() { return size == 0; }
 
-    // returns the value of the nth item (starting at 0 for first)
-    public E valueAt(int index) {
-        Node<E> p = getNode(index);
-        return p.val;
-    }
-
     // adds an item to the front of the list
     public void pushFront(E value) {
-        Node<E> frontNode = new Node<>(value);
+        Node<E> x = new Node<>(value);
         Node<E> next = head.next;
-        head.next = frontNode;
-        frontNode.next = next;
+        head.next = x;
+        x.next = next;
         size++;
     }
 
     // adds an item at the end
     public void pushBack(E value) {
         Node<E> newLastNode = new Node<>(value);
-        Node<E> lastNode = getNode(size-1);
+        Node<E> lastNode;
+        if (size - 1 >= 0){
+            lastNode = getNode(size - 1);
+        } else {
+            lastNode = head;
+        }
         lastNode.next = newLastNode;
+        newLastNode.next = tail;
         size++;
     }
 
     // insert value at index, so current item at that index is pointed to by new item at index
     public void insert(int index, E value) {
-        checkElementIndex(index);
+        checkPositionIndex(index);
+
+        // special case
+        if (index == size) {
+            pushBack(value);
+            return;
+        }
+
         Node<E> insertNode = new Node<>(value);
-        Node<E> nodeBeforeInsertion = getNode(index - 1);
+        Node<E> nodeBeforeInsertion;
+        if (index - 1 >= 0) {
+            nodeBeforeInsertion = getNode(index - 1);
+        } else {
+            nodeBeforeInsertion = head;
+        }
+
         Node<E> curIndexNode = nodeBeforeInsertion.next;
         nodeBeforeInsertion.next = insertNode;
         insertNode.next = curIndexNode;
@@ -58,17 +74,31 @@ public class MyLinkedList<E> {
 
     // remove front item and return its value
     public E popFront() {
+        if (empty()) {
+            throw new NoSuchElementException();
+        }
         Node<E> popNode = head.next;
         head.next = head.next.next;
+        popNode.next = null; // remember to set this to null
         size--;
         return popNode.val;
     }
 
     // removes end item and returns its value
     public E popBack() {
+        if (empty()) {
+            throw new NoSuchElementException();
+        }
+
         Node<E> popNode = getNode(size-1);
-        Node<E> newLastNode = getNode(size-2);
-        newLastNode.next = null;
+        Node<E> newLastNode;
+        if (size - 2 >= 0) {
+            newLastNode = getNode(size-2);
+        } else {
+            newLastNode = head;
+        }
+        newLastNode.next = tail;
+        popNode.next = null;
         size--;
         return popNode.val;
     }
@@ -76,18 +106,39 @@ public class MyLinkedList<E> {
     // removes node at given index
     public void erase(int index) {
         checkElementIndex(index);
-        Node<E> nodeBeforeErase = getNode(index - 1);
+
+        Node<E> nodeBeforeErase;
+        if (index - 1 >= 0) {
+            nodeBeforeErase = getNode(index - 1);
+        } else {
+            nodeBeforeErase = head;
+        }
+
+        Node<E> eraseNode = nodeBeforeErase.next;
         nodeBeforeErase.next = nodeBeforeErase.next.next;
+        eraseNode.next = null;
         size--;
+    }
+
+    // returns the value of the nth item (starting at 0 for first)
+    public E valueAt(int index) {
+        Node<E> p = getNode(index);
+        return p.val;
     }
 
     // get value of front item
     public E front() {
+        if (empty()) {
+            throw new NoSuchElementException();
+        }
         return head.next.val;
     }
 
     // get value of end item
     public E back() {
+        if (empty()) {
+            throw new NoSuchElementException();
+        }
         Node<E> backNode = getNode(size-1);
         return backNode.val;
     }
@@ -101,14 +152,18 @@ public class MyLinkedList<E> {
 
     // reverses the list
     public void reverse() {
-        Node<E> prev = null;
-        Node<E> curr = head;
+        Node<E> prev = head;
+        Node<E> curr = head.next;
         while (curr != null) {
             Node<E> nextTemp = curr.next;
             curr.next = prev;
             prev = curr;
             curr = nextTemp;
         }
+        Node<E> temp = head;
+        head = new Node<> (null);
+        head.next = prev;
+        tail = temp;
     }
 
     // removes the first item in the list with this value
@@ -135,9 +190,16 @@ public class MyLinkedList<E> {
         return p;
     }
     private boolean isElementIndex(int index){ return index >= 0 && index < size; }
+    private boolean isPositionIndex(int index){ return index >= 0 && index <= size; }
 
     private void checkElementIndex(int index) {
         if (!isElementIndex(index))
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+    }
+
+
+    private void checkPositionIndex(int index) {
+        if (!isPositionIndex(index))
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
     }
 
@@ -147,6 +209,7 @@ public class MyLinkedList<E> {
         for (int i = 1; i <= 5; i++) {
             list.pushFront(i);
         }
+        list.reverse();
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.valueAt(i));
         }
